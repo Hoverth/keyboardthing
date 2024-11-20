568e400b47b4d1635cc1f78c216ba3bc70d28e4c
Thomas Dickson <modscrat+github@gmail.com>
2024-11-20 14:34:26 +1300

    feat: sling now commucates with wayland (sort of)

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Keyboard Thing
 
 An attempt to create a virtual keyboard on wayland using rust.
+
+## Dev Notes
+
+- Plasma only advertises virtual keyboard interfaces to applications that advertise it (see `keyboardthing.desktop`)
+-
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
 // Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::{any::Any, error::Error};
+use std::error::Error;
+use std::sync::{Arc, Mutex};
 
 slint::include_modules!();
 
-use wayland_client::{protocol::wl_registry, Connection, Dispatch, Proxy, QueueHandle};
-// This struct represents the state of our app. This simple app does not
-// need any state, but this type still supports the `Dispatch` implementations.
+use slint::ComponentHandle;
+use wayland_client::{protocol::wl_registry, Connection, Dispatch, EventQueue, QueueHandle};
+use wayland_protocols::wp::input_method::zv1::client::zwp_input_method_v1;
+
+#[derive(Clone)]
 struct AppData {
+    output: String,
     use_kde: bool,
     use_generic: bool,
 }
 
-// Implement `Dispatch<WlRegistry, ()> for our state. This provides the logic
-// to be able to process events for the wl_registry interface.
-//
-// The second type parameter is the user-data of our implementation. It is a
-// mechanism that allows you to associate a value to each particular Wayland
-// object, and allow different dispatching logic depending on the type of the
-// associated value.
-//
-// In this example, we just use () as we don't have any value to associate. See
-// the `Dispatch` documentation for more details about this.
+impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppData {
+    fn event(
+        state: &mut Self,
+        _: &zwp_input_method_v1::ZwpInputMethodV1,
+        event: zwp_input_method_v1::Event,
+        _: &(),
+        _: &Connection,
+        _: &QueueHandle<AppData>,
+    ) {
+        state.output += format!("{event:?}").as_str();
+    }
+}
+
 impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
     fn event(
         state: &mut Self,
@@ -32,23 +39,24 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
         _: &Connection,
         _: &QueueHandle<AppData>,
     ) {
-        // When receiving events from the wl_registry, we are only interested in the
-        // `global` event, which signals a new available global.
-        // When receiving this event, we just print its characteristics in this example.
         if let wl_registry::Event::Global {
             name,
             interface,
             version,
         } = event
         {
-            println!("[{}] {} (v{})", name, interface, version);
+            state.output += format!("[{}] {} (v{})\n", name, interface, version).as_str();
             match interface.as_str() {
+                "zwp_input_method_v1" => {
+                    state.output += "Found string support!\n";
+                }
                 "org_kde_kwin_fake_input" => {
+                    state.output += "Found KDE!\n";
                     println!("KDE found!");
                     state.use_kde = true;
                 }
                 "zwp_virtual_keyboard_v1" => {
-                    println!("Generic Compositor found!");
+                    state.output += "Found generic!\n";
                     state.use_generic = !state.use_kde; // kde overrides this value
                 }
                 _ => {}
@@ -58,31 +66,20 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut app_data = AppData {
+    let app_data = AppData {
+        output: String::new(),
         use_kde: false,
         use_generic: false,
     };
 
+    let appdata = Arc::new(Mutex::new(app_data));
+
     // from https://docs.rs/wayland-client/latest/wayland_client/index.html
     let conn = Connection::connect_to_env().unwrap();
     let display = conn.display();
-    let mut event_queue = conn.new_event_queue();
+    let mut event_queue: EventQueue<AppData> = conn.new_event_queue();
     let qh = event_queue.handle();
     let _registry = display.get_registry(&qh, ());
-    println!("Advertised globals:");
-    event_queue.roundtrip(&mut app_data).unwrap();
-
-    use wayland_protocols_plasma::fake_input::client::org_kde_kwin_fake_input as pkde;
-
-    let fake_in = pkde::OrgKdeKwinFakeInput::inert(conn.backend().downgrade());
-
-    // either of these panic it, see: <https://github.com/Smithay/wayland-rs/blob/a078c5b36a8f68c095ffb2ad32dba7c2eef22310/wayland-backend/src/sys/client_impl/mod.rs#L505>
-
-    fake_in.keyboard_key(0, 3);
-    fake_in.authenticate(
-        String::from("keyboardthing"),
-        String::from("Emulate keyboard"),
-    );
 
     let ui = AppWindow::new()?;
 
@@ -91,6 +88,20 @@ fn main() -> Result<(), Box<dyn Error>> {
         move || {
             let ui = ui_handle.unwrap();
             ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.on_request_reload({
+        let ui_handle = ui.as_weak();
+        let appdata = appdata.clone();
+        move || {
+            appdata.clone().lock().unwrap().output += "---\n\n";
+            event_queue
+                .roundtrip(&mut appdata.clone().lock().unwrap())
+                .unwrap();
+
+            let ui = ui_handle.unwrap();
+            ui.set_output(appdata.clone().lock().unwrap().output.clone().into());
         }
     });
 
