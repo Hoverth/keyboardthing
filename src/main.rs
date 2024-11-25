3ce944657c6c8312315548cad67199c997534f88
Thomas Dickson <modscrat+github@gmail.com>
2024-11-25 16:19:55 +1300

    feat: fix crashing immediately, with help from logging panics to a file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use flexi_logger::{FileSpec, Logger};
 use slint::ComponentHandle;
 use std::error::Error;
 use std::sync::{Arc, Mutex};
+use wayland_client::event_created_child;
 use wayland_client::{protocol::wl_registry, Connection, Dispatch, EventQueue, QueueHandle};
 use wayland_protocols::wp::input_method::zv1::client::{
     zwp_input_method_context_v1, zwp_input_method_v1,
@@ -27,10 +29,15 @@ impl Dispatch<zwp_input_method_context_v1::ZwpInputMethodContextV1, ()> for AppD
         _: &QueueHandle<AppData>,
     ) {
         state.output += format!("Context: {event:?}").as_str();
+        log::info!("Context: {event:?}");
     }
 }
 
 impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppData {
+    event_created_child!(AppData, zwp_input_method_v1::ZwpInputMethodV1, [
+        0 => (zwp_input_method_context_v1::ZwpInputMethodContextV1, ())
+    ]);
+
     fn event(
         state: &mut Self,
         _: &zwp_input_method_v1::ZwpInputMethodV1,
@@ -40,11 +47,14 @@ impl Dispatch<zwp_input_method_v1::ZwpInputMethodV1, ()> for AppData {
         _: &QueueHandle<AppData>,
     ) {
         state.output += format!("Event: {event:?}").as_str();
+        log::info!("Event: {event:?}");
         if let zwp_input_method_v1::Event::Activate { id } = event {
             state.output += format!("Activated: {id:?}").as_str();
+            log::info!("Activated: {id:?}");
             state.input_context = Some(id);
         } else if let zwp_input_method_v1::Event::Deactivate { context } = event {
             state.output += format!("Deactivated: {context:?}").as_str();
+            log::info!("Deactivated {context:?}");
             if let Some(input_context) = &state.input_context {
                 if context == *input_context {
                     state.input_context = None;
@@ -71,14 +81,17 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
         } = event
         {
             state.output += format!("[{}] {} (v{})\n", name, interface, version).as_str();
+            log::info!("[{}] {} (v{})", name, interface, version);
             match interface.as_str() {
                 "zwp_input_method_v1" => {
                     state.output += "Found string support!\n";
+                    log::info!("Found string support (zwp_input_method_v1)");
                     registry.bind::<zwp_input_method_v1::ZwpInputMethodV1, _, _>(name, 1, qh, ());
                 }
                 "org_kde_kwin_fake_input" => {
                     state.output += "Found KDE!\n";
                     println!("KDE found!");
+                    log::info!("Found KDE fake input (org_kde_kwin_fake_input)");
                     state.use_kde = true;
                 }
                 "zwp_virtual_keyboard_v1" => {
@@ -92,6 +105,14 @@ impl Dispatch<wl_registry::WlRegistry, ()> for AppData {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
+    log_panics::init();
+    Logger::try_with_str("info")?
+        .log_to_file(FileSpec::try_from(/*add your log path here*/ "")?)
+        .append()
+        .format(flexi_logger::detailed_format)
+        .start()?;
+    log::info!("Started logging!\n\n");
+
     let app_data = AppData {
         output: String::new(),
         use_kde: false,
@@ -119,6 +140,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     });
 
     ui.on_request_reload({
+        log::info!("Wayland reload requested");
         let ui_handle = ui.as_weak();
         let appdata = appdata.clone();
         move || {
@@ -129,6 +151,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             let ui = ui_handle.unwrap();
             ui.set_output(appdata.clone().lock().unwrap().output.clone().into());
+        }
+    });
+
+    ui.on_add_text({
+        let ui_handle = ui.as_weak();
+        let appdata = appdata.clone();
+        move |text| {
+            log::info!("Trying to add text!: {text:?}");
+
+            //appdata.clone().lock().unwrap().input_context.whatever()
+
+            let ui = ui_handle.unwrap();
+            ui.invoke_request_reload();
         }
     });
 
